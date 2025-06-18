73e732c534fbedefc16e31e082cc55485cb0c16f
Toni Lubiniecki <hidden.toni@pm.me>
2025-06-18 19:52:53 +0200

    hyprlock adapted for laptop

--- hyprland/.config/hypr/scripts/power.sh
+++ hyprland/.config/hypr/scripts/power.sh
@@ -7,59 +7,59 @@ CHARGE_ICON="󰂄"
 FULL_ICON="󱟢"
 POWERED_ICON="󰚥"
 
-# Check if battery directory exists
 has_battery() {
     [[ -d "/sys/class/power_supply/$BATTERY_NAME" ]]
 }
 
-# Get battery state
 battery_state() {
-    <"/sys/class/power_supply/$BATTERY_NAME/status"
+    cat "/sys/class/power_supply/$BATTERY_NAME/status"
 }
 
-# Get battery percentage
 battery_soc() {
-    <"/sys/class/power_supply/$BATTERY_NAME/capacity"
+    cat "/sys/class/power_supply/$BATTERY_NAME/capacity"
 }
 
-# Get battery icon based on charge state
 battery_icon() {
     if ! has_battery; then
         echo "$POWERED_ICON"
         return
     fi
 
-    case "$(battery_state)" in
-    "Charging")
-        echo "$CHARGE_ICON"
-        ;;
-    "Full")
-        echo "$FULL_ICON"
-        ;;
-    *)
-        local soc
-        soc=$(battery_soc)
-        local idx=$((soc / 10))
-        echo "${SOC_ICONS[idx]}"
-        ;;
+    local state
+    state=$(battery_state)
+
+    case "$state" in
+        "Charging")
+            echo "$CHARGE_ICON"
+            ;;
+        "Full")
+            echo "$FULL_ICON"
+            ;;
+        *)
+            local soc
+            soc=$(battery_soc)
+            local idx=$((soc / 10))
+            ((idx > 9)) && idx=9
+            echo "${SOC_ICONS[idx]}"
+            ;;
     esac
 }
 
 case "$1" in
---icon | -i)
-    battery_icon
-    ;;
---value | -v)
-    if ! has_battery; then
-        echo ""
-    else
-        echo "$(battery_soc)%"
-    fi
-    ;;
-*)
-    echo "Usage:
-    $0 {-i|--icon}
-    $0 {-v|--value}"
-    exit 1
-    ;;
+    --icon | -i)
+        battery_icon
+        ;;
+    --value | -v)
+        if has_battery; then
+            echo "$(battery_soc)%"
+        else
+            echo ""
+        fi
+        ;;
+    *)
+        echo "Usage:
+    $0 {-i|--icon}   # Show battery icon
+    $0 {-v|--value}  # Show battery percentage"
+        exit 1
+        ;;
 esac

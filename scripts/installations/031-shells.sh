73c9a6e38a30689397e05badd64cf8c83912c860
Toni <hidden.toni@pm.me>
2025-10-21 18:47:19 +0200

    further adaptations for dynamic coloring

--- scripts/installations/031-shells.sh
+++ scripts/installations/031-shells.sh
@@ -27,6 +27,27 @@ install() {
     safe_symlink .config/noctalia
     safe_symlink .config/caelestia
     safe_symlink .config/matugen
+
+    ln -s ~/.local/state/caelestia/theme/kitty.conf ~/Dotfiles/.config/kitty/themes/caelestia.conf
+    ln -s ~/.local/state/caelestia/theme/zed.json ~/Dotfiles/.config/zed/themes/caelestia.json
+
+    # Adapting `caelestia-cli` package
+    pypkg_dir=/usr/lib64/python3.13/site-packages/caelestia/
+
+    theme_script=$pypkg_dir/utils/theme.py
+    discord_template=$pypkg_dir/data/templates/discord.scss
+
+    # Different icons for gtk
+    sudo sed -E -i 's/Papirus-\{mode\.capitalize\(\)\}/Win11/g' "$theme_script"
+
+    # Disable config generation for unused Discord clients
+    sudo sed -E -i 's/"Equicord", "Vencord", "BetterDiscord", "equibop", "vesktop", "legcord"/["vesktop"]/g' "$theme_script"
+
+    # Bigger gap for discord title bar. Propably caused by my high fractional scaling.
+    sudo sed -z -E -i 's/(--top-bar-height:[[:space:]]*)var\([[:space:]]*--gap[[:space:]]*\)/\1var(--gap * 2)/g' "$discord_template"
+
+    # Change font
+    sudo sed -i 's/--font: "figtree";/--font: "Monaspace Krypton";/' "$discord_template"
 }
 
 uninstall() {

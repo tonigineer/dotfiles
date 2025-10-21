6ab72717cf5d1f97b7e95cfd0b2726e9e667f826
Toni <hidden.toni@pm.me>
2025-10-21 21:38:27 +0200

    remove cava; adapting cava template of caelestia

--- scripts/installations/013-terminal.sh
+++ scripts/installations/013-terminal.sh
@@ -28,7 +28,6 @@ install() {
     yay_install "${pkgs_tools[@]}"
 
     safe_symlink .config/btop
-    safe_symlink .config/cava
     safe_symlink .config/fastfetch
     safe_symlink .config/kitty
     safe_symlink .config/yazi
@@ -38,7 +37,15 @@ install() {
     ya pkg add yazi-rs/plugins:no-status
     ya pkg add yazi-rs/plugins:git
     ya pkg add yazi-rs/plugins:smart-enter
-    ya pkg add marcosvnmelo/kanagawa-dragon
+    # ya pkg add marcosvnmelo/kanagawa-dragon
+
+    local CAVA_TEMPLATE=/usr/lib64/python3.13/site-packages/caelestia/data/templates/cava.conf
+    sudo sed -i 's/bars = 64/max_height = 80\
+bar_width = 1\
+bar_spacing = 0\
+sensitivity = 50/' "$CAVA_TEMPLATE"
+    sudo sed -i 's/gravity = 120/gravity = 50/' "$CAVA_TEMPLATE"
+    sudo sed -i 's/framerate = 60/framerate = 80/' "$CAVA_TEMPLATE"
 }
 
 uninstall() {
@@ -46,7 +53,6 @@ uninstall() {
     yay_uninstall "${pkgs[@]}"
 
     rm -rf ~/.config/btop
-    rm -rf ~/.config/cava
     rm -rf ~/.config/fastfetch
     rm -rf ~/.config/kitty
     rm -rf ~/.config/yazi

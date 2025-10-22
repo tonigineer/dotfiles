07f30ac27a672f4cc0fa5400fe7453503f160439
Toni <hidden.toni@pm.me>
2025-10-22 12:54:48 +0200

    librewolf dynamic linking added

--- scripts/installations/041-browser.sh
+++ scripts/installations/041-browser.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+readonly COLORSCHEME_DIR=~/.local/state/caelestia/theme/librewolf.css
+
 pkgs=(
     librewolf-bin
     xdg-desktop-portal
@@ -7,16 +9,32 @@ pkgs=(
     xdg-desktop-portal-hyprland
 )
 
+get_profile() {
+  local ini="$HOME/.librewolf/profiles.ini" def
+  def=$(awk -F= '/^\[Install/ {i=1} i && $1=="Default" {print $2; exit}' "$ini")
+  if [[ -n $def ]]; then printf '%s/.librewolf/%s\n' "$HOME" "$def"
+  else
+    awk -F= -v d="$HOME/.librewolf" '
+      /^\[Profile/ {p=1} p&&$1=="Path"{path=$2}
+      p&&$1=="Default"&&$2==1{print d"/"path; exit}
+    ' "$ini"
+  fi
+}
+
+readonly PROFILE_DIR=$(get_profile)
+
 status() {
     yay_check "${pkgs[@]}" &&
-        [ -L ~/.librewolf/colorscheme.css ]
+        [ -L ~/.librewolf/colorscheme.css ] &&
+        [ -L $PROFILE_DIR/chrome/userChrome.css ]
 }
 
 install() {
     yay_install "${pkgs[@]}"
 
-    safe_symlink .librewolf/colorscheme.css
-    # Todo: Find current profile and link userChrome.css
+    mkdir -p $PROFILE_DIR/chrome
+    ln -sf $dotfiles_dir/.librewolf/userChrome.css $PROFILE_DIR/chrome/userChrome.css
+    ln -sf "$COLORSCHEME_DIR" ~/.librewolf/colorscheme.css
 
     xdg-settings set default-web-browser librewolf.desktop
 }
@@ -25,5 +43,6 @@ uninstall() {
     local pkgs=(librewolf-bin)
     yay_uninstall "${pkgs[@]}"
 
-    rm -rf ~/.librewolf/colorscheme.css
+    rm -f $PROFILE_DIR/chrome/userChrome.css
+    rm -f ~/.librewolf/colorscheme.css
 }

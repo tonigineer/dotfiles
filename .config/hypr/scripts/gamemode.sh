504a1583521fac4e6ab6fdb91b788b1a2148d6d6
Toni <hidden.toni@pm.me>
2025-11-08 19:10:34 +0100

    collection of small changes

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
+#
+
+readonly CFG_FILE=~/.config/caelestia/shell.json
 
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
+    echo "$( jq --indent 4 '.border.rounding = 0 | .border.thickness = 0' "$CFG_FILE" )" > "$CFG_FILE"
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword animation borderangle,0; \
@@ -10,12 +14,13 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
 	    keyword decoration:fullscreen_opacity 1;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
+        keyword general:border_size 0;\
         keyword decoration:rounding 0"
     hyprctl notify 1 5000 "rgb(40a02b)" "Gamemode [ON]"
     exit
 else
     hyprctl notify 1 5000 "rgb(d20f39)" "Gamemode [OFF]"
+    echo "$( jq --indent 4 '.border.rounding = 25 | .border.thickness = 10' $CFG_FILE)" > "$CFG_FILE"
     hyprctl reload
     exit 0
 fi
